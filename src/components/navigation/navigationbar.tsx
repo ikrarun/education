0579fa0b80614b80373df35ce8b5d40af83a719f
Arun <iamkrarun@gmail.com>
2024-12-02 19:50:06 +0530

    update navigationbar.tsx

--- src/components/navigation/navigationbar.tsx
+++ src/components/navigation/navigationbar.tsx
@@ -6,9 +6,9 @@ function navigationbar() {
 	return (
 		<div className='sticky top-0  inline-flex w-full z-50 bg-black text-white shadow-sm'>
 			<div className='bg-black text-white max-w-[1200px] mx-auto shadow-sm w-full p-4 inline-flex justify-between items-center'>
-				<Link className="inline-flex font-bold gap-2 items-center" href='/'>
+				<Link className='inline-flex font-bold gap-2 items-center' href='/'>
 					<Image src={logo} width={30} height={30} alt='logo' />
-					<h1 className='text-2xl font-normal'>Education</h1>
+					<h1 className='text-2xl font-normal'>EduKation</h1>
 				</Link>
 				<div className='inline-flex justify-center items-center gap-4'>
 					<Link href='/about'>
