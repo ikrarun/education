6803fe53d6d6839df5389afe5e1c0c7fe5f4148d
Arun <iamkrarun@gmail.com>
2024-12-07 04:15:16 +0530

    update next.config.ts removed sentry

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
+	compress: true,
+	i18n: {
+		locales: ["en", "in"],
+		defaultLocale: "en",
+	},
 };
 
 export default nextConfig;
