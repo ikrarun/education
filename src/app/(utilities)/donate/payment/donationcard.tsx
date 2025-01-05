565e1b52c95d1685f67ca8098f02e4e68efe1489
Arun <thewiseconcern@gmail.com>
2025-01-06 00:18:29 +0530

    Update Cashfree payment mode configuration for environment-specific settings
    - Modified the Cashfree payment initialization in `donationcard.tsx` to set the mode based on the current environment, using "sandbox" for development and "production" for other environments.
    - This change enhances the flexibility and safety of the payment integration by ensuring that the appropriate mode is used based on the deployment context.
    
    These changes collectively improve the configuration management of the payment system within the application.

--- src/app/(utilities)/donate/payment/donationcard.tsx
+++ src/app/(utilities)/donate/payment/donationcard.tsx
@@ -9,36 +9,36 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-} from "@/components/ui/dialog";
-import { Tabs, TabsTrigger, TabsList, TabsContent } from "@/components/ui/tabs";
+
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
-import { useState, useEffect, useRef, SetStateAction, Dispatch } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getPaymentSessionId } from "./paymentBackend";
 import { CopyIcon, EyeIcon } from "lucide-react";
-
+import { toast } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 const predefinedAmounts = [1, 5, 10, 20, 50, 100];
 
 const DonationCard = () => {
-	// State Variables
-	const [donationAmount, setDonationAmount] = useState(1);
+	// Cashfree component
 	const cashfree = useRef<unknown>(null);
+
+	// Donation Amount
+	const [donationAmount, setDonationAmount] = useState<number | undefined>(
+		undefined
+	);
+
+	// Payment Session Id
 	const [paymentSessionId, setPaymentSessionId] = useState<string | undefined>(
 		undefined
 	);
+	const [isPaymentShown, setIsPaymentShown] = useState(false);
+
+	// Order Id
 	const [orderId, setOrderId] = useState<string | undefined>(undefined);
-	const [paymentMethod, setPaymentMethod] = useState<
-		"upi" | "debit" | undefined
-	>(undefined);
-	const [isOpen, setIsOpen] = useState<boolean>(true);
+	const [paymentMethod, setPaymentMethod] = useState<"upi" | "debit">("upi");
+	// Donation Date
 	const [donationDate] = useState(
 		new Date().toLocaleDateString("en-GB", {
 			day: "2-digit",
@@ -48,93 +48,118 @@ const DonationCard = () => {
 	);
 
 	// QR Code Ref
-	const qrCodeRef = useRef<HTMLDivElement>(null);
-	const paymentComponent = useRef<unknown>(null);
 
-	// Pre Check for Cashfree
-	const preCheck = () => {
+	// Cashfree Component is Rendered here
+	const renderQRCodehere = useRef<unknown>(null);
+
+	// Test Is The Cashfree is initialized
+	const checkIsCashfreeInitialized = () => {
 		if (!cashfree.current) {
-			console.log("Cashfree not initialized , Now initializing");
+			toast.info("Cashfree not initialized , Now initializing");
 			cashfree.current = window.Cashfree({
-				mode: "sandbox",
+				mode: process.env.NODE_ENV === "development" ? "sandbox" : "production",
 			});
 		}
 
 		if (!cashfree.current) {
-			console.warn("Cashfree could not be initialized");
+			toast.error("Cashfree could not be initialized");
 			return false;
 		}
 		return true;
 	};
 
-	// Get Payment Session Id
+	// We get the Payment Session Id from the backend when the donation amount is set;
 	useEffect(() => {
-		getPaymentSessionId({ amount: donationAmount }).then((response) => {
+		if (donationAmount === undefined) {
+			toast.info("Donation Amount is undefined");
+			return;
+		}
+	}, [donationAmount]);
+
+	// Fetch Session Id from the backend
+	const fetchSessionId = async () => {
+		if (donationAmount === undefined) {
+			toast.info("Donation Amount is undefined");
+			return;
+		}
+		await getPaymentSessionId({ amount: donationAmount }).then((response) => {
 			if (response !== undefined) {
 				console.log(response);
 				setPaymentSessionId(response.payment_session_id);
 				setOrderId(response.order_id);
 			} else {
-				console.log("Error");
+				toast.error("Error");
 			}
 		});
-	}, [isOpen, donationAmount]);
+	};
 
-	// Initialize Cashfree
+	// We initialize the Cashfree component when the page is loaded , this is done only once
 	useEffect(() => {
 		cashfree.current = window.Cashfree({
 			mode: "sandbox",
 		});
-		preCheck();
 	}, []);
 
-	useEffect(() => {
-		if (paymentMethod === undefined) {
+	// Render Payment Component on Payment Method Change
+	const renderPaymentComponent = ({
+		modeofPayment = "upi",
+	}: {
+		modeofPayment?: "upi" | "debit";
+	} = {}) => {
+		const qrCodeContainer = document.getElementById("qr-code-container");
+		if (!checkIsCashfreeInitialized()) {
+			toast.error("Cashfree was not initialized so cannot proceed");
 			return;
 		}
-		if (!preCheck()) {
-			console.log("Cashfree was not initialized so cannot proceed");
+		if (qrCodeContainer === null) {
+			toast.info("QR Code Container is not found");
 			return;
 		}
-		if (paymentMethod === "upi") {
+		if (modeofPayment === "upi") {
+			renderQRCodehere.current =
+				// @ts-expect-error Cashfree is not typed
+				cashfree.current?.create("upiQr", {
+					values: {
+						size: "220px",
+					},
+				});
 			// @ts-expect-error Cashfree is not typed
-			paymentComponent.current = cashfree.current?.create("upiQr", {
-				values: {
-					size: "220px",
-				},
+			renderQRCodehere.current.on("loaderror", (data: unknown) => {
+				toast.error(data as string);
 			});
-			// @ts-expect-error Cashfree is not typed
-			paymentComponent.current.on("loaderror", (data: unknown) => {
-				console.error(data);
-			});
-			// @ts-expect-error Cashfree is not typed
-			paymentComponent.current.mount(qrCodeRef.current);
-			// @ts-expect-error Cashfree is not typed
-			paymentComponent.current.on("load", (data: unknown) => {
-				console.log(data);
-			});
-		}
-	}, [paymentMethod]);
 
-	const performPaymentSetup = () => {
-		console.log("Performing payment setup");
-		if (!preCheck()) {
-			console.log("Cashfree was not initialized so cannot proceed");
-			return;
+			// @ts-expect-error Cashfree is not typed
+			renderQRCodehere.current.mount(qrCodeContainer);
+
+			// @ts-expect-error Cashfree is not typed
+			renderQRCodehere.current.on("load", (data: unknown) => {
+				toast.info(data as string);
+			});
 		}
-		setIsOpen(true);
-		console.log("changed the Open State");
 	};
 
+	// Open Payment Dialog
+	const startSettingUpPayment = () => {
+		toast.info("Performing payment setup");
+		if (!checkIsCashfreeInitialized()) {
+			toast.error("Cashfree was not initialized so cannot proceed");
+			return;
+		}
+		fetchSessionId();
+		setIsPaymentShown(true);
+		renderPaymentComponent();
+	};
+
+	// Pay Now
 	const payNow = async () => {
-		console.log("Paying Now");
-		if (!preCheck()) {
-			console.log("Cashfree was not initialized so cannot proceed");
+		toast.info("Paying Now");
+		if (!checkIsCashfreeInitialized()) {
+			toast.error("Cashfree was not initialized so cannot proceed");
 			return;
 		}
 		if (paymentMethod === "upi") {
 			const PaymentOptions = {
-				paymentMethod: paymentComponent.current,
+				paymentMethod: renderQRCodehere.current,
 				paymentSessionId: paymentSessionId,
 				returnUrl:
 					process.env.NODE_ENV === "development"
@@ -145,16 +170,16 @@ const DonationCard = () => {
 			cashfree.current?.pay(PaymentOptions);
 			return true;
 		} else {
-			console.log("Payment Method is not set");
+			toast.error("Payment Method is not set");
 			return false;
 		}
 	};
 
 	return (
-		<>
-			<Card
-				style={{ fontFamily: inter.style.fontFamily }}
-				className='w-full my-4 p-4 gap-4 flex flex-col max-w-[400px]'>
+		<div
+			className='flex flex-col w-fit gap-2 max-w-[400px]'
+			style={{ fontFamily: inter.style.fontFamily }}>
+			<Card className='w-full my-4 p-4 gap-4 flex flex-col '>
 				<CardHeader className='p-0 '>
 					<CardTitle>Donation Details</CardTitle>
 					<CardDescription>
@@ -181,7 +206,7 @@ const DonationCard = () => {
 						))}
 					</div>
 
-					<Button className='w-full mt-2' onClick={performPaymentSetup}>
+					<Button className='w-full mt-2' onClick={startSettingUpPayment}>
 						Donate
 					</Button>
 				</CardContent>
@@ -191,147 +216,118 @@ const DonationCard = () => {
 					</CardDescription>
 				</CardFooter>
 			</Card>
-			<PaymentDialog
-				donationAmount={donationAmount}
-				open={isOpen}
-				setOpen={setIsOpen}
-				paymentSessionId={paymentSessionId}
-				qrCodeRef={qrCodeRef}
-				setPaymentMethod={setPaymentMethod}
-				payNow={payNow}
-			/>
-		</>
-	);
-};
 
-function PaymentDialog({
-	donationAmount,
-	open,
-	setOpen,
-	paymentSessionId,
-	qrCodeRef,
-	setPaymentMethod,
-	payNow,
-}: {
-	donationAmount: number;
-	open: boolean;
-	setOpen: Dispatch<SetStateAction<boolean>>;
-	paymentSessionId: string | undefined;
-	qrCodeRef: React.RefObject<HTMLDivElement | null>;
-	setPaymentMethod: Dispatch<SetStateAction<"upi" | "debit" | undefined>>;
-	payNow: () => Promise<boolean | undefined>;
-}) {
-	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogContent
-				onInteractOutside={(e) => {
-					e.preventDefault();
-				}}
-				style={{ fontFamily: inter.style.fontFamily }}>
-				<DialogHeader>
-					<DialogTitle>Are you sure you want to donate?</DialogTitle>
-					<DialogDescription>
-						Since you are donating to a non-profit organization, your donation
-						will be used to support the development of the platform.
-					</DialogDescription>
-				</DialogHeader>
-
-				<div className='flex text-sm mt-6 flex-col gap-1'>
-					<h1>You are donating ₹ {donationAmount}</h1>
-					<h1 className='text-muted-foreground text-xs'>
+			{/* Dialog for Payment */}
+			<Card
+				className={cn(
+					"w-full my-4 p-4 gap-4 flex flex-col ",
+					isPaymentShown ? "block" : "scale-0"
+				)}>
+				<CardHeader className='p-0'>
+					<CardTitle>You are donating ₹ {donationAmount}</CardTitle>
+					<CardDescription>
 						We have multiple payment option kindly select on which suits you
 						better.
-					</h1>
-					<Tabs
-						defaultValue='upi'
-						className='min-w-xl gap-1 h-full w-full flex flex-row'>
-						{/* Tabs List */}
-						<div className='w-fit flex flex-col h-full'>
-							<TabsList className='w-fit flex flex-col h-fit mt-2 mr-1'>
-								<TabsTrigger
-									onClick={() => setPaymentMethod("upi")}
-									className='w-full'
-									value='upi'>
-									UPI
-								</TabsTrigger>
-								<TabsTrigger
-									onClick={() => setPaymentMethod("debit")}
-									className='w-full'
-									value='debit'>
-									Debit Card
-								</TabsTrigger>
-							</TabsList>
-						</div>
-						{/* Tabs Content is Here */}
-						<TabsContent value='upi' className='w-full h-full'>
-							<Card className='w-full p-0 h-full'>
-								<CardHeader className='p-4 pb-0'>
-									<CardTitle>Scan QR Code</CardTitle>
-									<CardDescription>
-										We have the easiest solution to pay now
-									</CardDescription>
-								</CardHeader>
-								<CardContent className='p-4 pb-2'>
-									{paymentSessionId ? (
-										<div className='flex flex-col gap-1'>
-											<div
-												className={cn(
-													"mt-2 text-muted-foreground inline-flex items-center gap-1",
-													paymentSessionId ? "" : "hidden"
-												)}>
-												Click to copy the payment Id
-												<CopyIcon
-													onClick={() => {
-														navigator.clipboard.writeText(
-															paymentSessionId ?? ""
-														);
-													}}
-													size={12}
-													className='cursor-pointer'
-												/>
-											</div>
-											<div className='w-fit mx-auto  flex-col relative h-full flex justify-center items-center'>
-												<div ref={qrCodeRef} className='h-full max-w-[220px]' />
-												<Button
-													id='reveal-qr-code'
-													variant={"outline"}
-													onClick={() => {
-														payNow().then((res) => {
-															if (res) {
-																const revealButton =
-																	document.getElementById("reveal-qr-code");
-																revealButton?.classList.add("hidden");
-															}
-														});
-													}}
-													className='absolute rounded-full mx-auto'>
-													<EyeIcon size={12} />
-												</Button>
-											</div>
-										</div>
-									) : (
-										<div className='mt-2 animate-pulse transition-all duration-1000 text-muted-foreground inline-flex items-center gap-1'>
-											Loading...
-										</div>
-									)}
-								</CardContent>
-							</Card>
-						</TabsContent>
-						<TabsContent value='debit' className='w-full h-full'>
-							<Card className='w-full h-full'>
-								<CardHeader>
-									<CardTitle>Hi I am Password</CardTitle>
-								</CardHeader>
-								<CardContent>
-									<CardDescription>Hi I am Password</CardDescription>
-								</CardContent>
-							</Card>
-						</TabsContent>
-					</Tabs>
-				</div>
-			</DialogContent>
-		</Dialog>
+					</CardDescription>
+				</CardHeader>
+				{/* Tabs List */}
+
+				<CardContent className='p-0 flex flex-col gap-2 mt-4'>
+					<div className='w-full inline-flex mt-4 gap-1 '>
+						<Button
+							onClick={() => {
+								setPaymentMethod("upi");
+								renderPaymentComponent({ modeofPayment: "upi" });
+							}}
+							className='w-full'
+							variant={paymentMethod === "upi" ? "default" : "outline"}
+							value='upi'>
+							UPI
+						</Button>
+						<Button
+							onClick={() => {
+								setPaymentMethod("debit");
+								renderPaymentComponent({ modeofPayment: "debit" });
+							}}
+							className='w-full'
+							variant={paymentMethod === "debit" ? "default" : "outline"}
+							value='debit'>
+							Debit Card
+						</Button>
+					</div>
+
+					{/* Tabs Content is Here */}
+					<Card
+						className={cn(
+							"w-full p-0 h-full",
+							paymentMethod === "upi" ? "block" : "hidden"
+						)}>
+						<CardHeader className='p-4 pb-0'>
+							<CardTitle>Scan QR Code</CardTitle>
+							<CardDescription>
+								We have the easiest solution to pay now
+							</CardDescription>
+						</CardHeader>
+						<CardContent className='p-4 pb-2'>
+							{
+								<div className={cn("flex flex-col gap-1 ")}>
+									<div
+										className={cn(
+											"mt-2 text-muted-foreground inline-flex items-center gap-1",
+											paymentSessionId ? "" : "scale-0"
+										)}>
+										Click to copy the payment Id
+										<CopyIcon
+											onClick={() => {
+												navigator.clipboard.writeText(paymentSessionId ?? "");
+											}}
+											size={12}
+											className='cursor-pointer'
+										/>
+									</div>
+									<div className='w-fit mx-auto  flex-col relative h-full flex justify-center items-center'>
+										<div
+											id='qr-code-container'
+											className='h-full max-w-[220px]'
+										/>
+										<Button
+											id='reveal-qr-code'
+											variant={"outline"}
+											onClick={() => {
+												payNow().then((res) => {
+													if (res) {
+														const revealButton =
+															document.getElementById("reveal-qr-code");
+														revealButton?.classList.add("scale-0");
+													}
+												});
+											}}
+											className='absolute rounded-full mx-auto'>
+											<EyeIcon size={12} />
+										</Button>
+									</div>
+								</div>
+							}
+						</CardContent>
+					</Card>
+
+					<Card
+						className={cn(
+							"flex flex-col gap-1 ",
+							paymentMethod === "debit" ? "block" : "hidden"
+						)}>
+						<CardHeader>
+							<CardTitle>Debit Card</CardTitle>
+							<CardDescription>Pay using your debit card</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<Button>Pay Now</Button>
+						</CardContent>
+					</Card>
+				</CardContent>
+			</Card>
+		</div>
 	);
-}
+};
 
 export default DonationCard;
--- src/app/(utilities)/layout.tsx
+++ src/app/(utilities)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway as Font } from "next/font/google";
 import "@/app/globals.css";
 import Nav from "@/components/nav";
+import { Toaster } from "sonner";
 
 const font = Font({
 	subsets: ["latin"],
@@ -23,6 +24,7 @@ export default async function RootLayout({
 				className={`${font.className} flex flex-col min-h-screen antialiased`}>
 				<Nav />
 				{children}
+				<Toaster richColors />
 			</body>
 		</html>
 	);
