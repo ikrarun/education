93b003cf5e2b340dbade8be9417b766cb98049c8
Arun <thewiseconcern@gmail.com>
2025-01-11 14:40:07 +0530

    Refactor auth configuration to use dynamic baseURL from config file for improved environment handling

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,9 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 
 import { oneTap } from "better-auth/plugins";
 import { prisma } from "./db";
+import { baseURL } from "./config";
 export const auth = betterAuth({
-	baseURL: "http://localhost:3000", // the base url of your auth server
+	baseURL: baseURL, // the base url of your auth server
 	database: prismaAdapter(prisma, {
 		provider: "postgresql", // or "mysql", "postgresql", ...etc
 	}),
--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -1,8 +1,9 @@
 import { oneTapClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { baseURL } from "./config";
 
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:3000",
+	baseURL: baseURL,
 	plugins: [
 		oneTapClient({
 			clientId: process.env.NEXT_PUBLIC_AUTH_GOOGLE_ID!,
